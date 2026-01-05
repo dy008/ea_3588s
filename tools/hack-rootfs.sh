1548e09069c1102b25916f30e0847410d59aab8c
yifengyou <842056007@qq.com>
2026-01-05 14:46:05 +0000

    update hack-rootfs
    Signed-off-by: yifengyou <842056007@qq.com>

--- tools/hack-rootfs.sh
+++ tools/hack-rootfs.sh
@@ -2,17 +2,29 @@
 
 set -x
 
-
 if [ -f /lib/systemd/system/lightdm.service ];then
 	systemctl enable lightdm
+fi
+
+
+
+if [ -f /lib/systemd/system/gdm.service ] ; then
+	cat >> /lib/systemd/system/gdm.service << "EOF"
+
+[Install]
+WantedBy=graphical.target
+EOF
+
+	ln -svf /lib/systemd/system/gdm.service /etc/systemd/system/graphical.target.wants/gdm.service
 fi
 
 if [ -f /lib/systemd/system/gdm.service ];then
 	systemctl enable gdm
 fi
 
-chmod +x /etc/rc.local
+systemctl daemon-reload
 
+chmod +x /etc/rc.local
 
 sed -i 's/NanoPC T6/EA_3588S/g' /etc/armbian-*
 
